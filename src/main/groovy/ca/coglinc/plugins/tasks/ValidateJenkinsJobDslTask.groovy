819a7bd15500d298874ce68850520b5fac0d0305
John Martel <jonathan.martel@coglinc.ca>
2017-12-27 15:24:54 -0500

    Add validation tests from `validateJenkinsJobDsl` task
    Although it is quite simple and elegant, this approach does not seem to
    work as it is difficult to set the classpath from the `jenkins`
    configuration.
    
    This commit simply saves this step so that we can branch off this idea
    and come back to it later if other ideas do not work.

--- src/main/groovy/ca/coglinc/plugins/tasks/ValidateJenkinsJobDslTask.groovy
+++ src/main/groovy/ca/coglinc/plugins/tasks/ValidateJenkinsJobDslTask.groovy
@@ -3,12 +3,49 @@ package ca.coglinc.plugins.tasks
 import groovy.transform.PackageScope
 import org.gradle.api.tasks.SourceTask
 import org.gradle.api.tasks.TaskAction
+import org.junit.runner.Result
+import spock.util.EmbeddedSpecRunner
 
 @PackageScope
 class ValidateJenkinsJobDslTask extends SourceTask {
 
     @TaskAction
     void run() {
+        StringBuilder allTests = new StringBuilder()
+        source.forEach { File file ->
+            allTests.append("""
+                void 'Validate Jenkins Job DSL script ${file.absolutePath}'() {
+                    given:
+                    JenkinsJobManagement jobManagement = new JenkinsJobManagement(System.out, [:], new File('.'))
+            
+                    when:
+                    new DslScriptLoader(jobManagement).runScript(${file.text})
+            
+                    then:
+                    noExceptionThrown()
+                }
+                
+            """)
+        }
 
+        EmbeddedSpecRunner runner = new EmbeddedSpecRunner(throwFailure: false)
+        String validationTestSource = """
+            import javaposse.jobdsl.dsl.DslScriptLoader
+            import javaposse.jobdsl.plugin.JenkinsJobManagement
+            import org.junit.ClassRule
+            import org.jvnet.hudson.test.JenkinsRule
+            import spock.lang.Shared
+            import spock.lang.Specification
+            
+            class JobScriptsSpec extends Specification {
+                @Shared
+                @ClassRule
+                JenkinsRule jenkinsRule = new JenkinsRule()
+            
+                ${allTests.toString()}
+            }
+        """
+        Result result = runner.runWithImports(validationTestSource)
+        result.properties.each { k, v -> println "$k: $v" }
     }
 }
